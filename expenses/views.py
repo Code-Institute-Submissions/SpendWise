a9730fd333aaaf798eab2e845504c60656b996f0
Thomas-Longworth <thomaslongworth555@gmail.com>
2023-04-12 19:27:33 +0000

    rendered the expense object in the template

--- expenses/views.py
+++ expenses/views.py
@@ -1,12 +1,20 @@
 from django.shortcuts import render
-
+from .models import Expense
 # Create your views here.
 
 def home(request):
     return render(request, 'home_page.html')   
+
 def add(request):
     return render(request, 'add_expenses.html')   
+
 def update(request):
     return render(request, 'update_expenses.html')
+
 def exp_page(request):
-    return render(request, 'expenses_page.html')
+    expenses = Expense.objects.all()
+    context = {
+        'expenses': expenses
+    }
+    return render(request, 'expenses_page.html', context)
+
