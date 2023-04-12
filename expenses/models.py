5eb17df18a9fd9762523a009f420448fa8fc8fc3
Thomas-Longworth <thomaslongworth555@gmail.com>
2023-04-12 18:59:45 +0000

    made a new model for an expense

--- expenses/models.py
+++ expenses/models.py
@@ -1,3 +1,16 @@
-from django.db import models
 
+from django.db import models
+from django.core.validators import MaxValueValidator, MinValueValidator
+
+from datetime import datetime, date
 # Create your models here.
+
+class Expense(models.Model):
+    name = models.CharField( max_length=50, null = False, blank = False)
+    number = models.PositiveIntegerField(default=0,  validators=[MinValueValidator(1), MaxValueValidator(100000)])
+    date_field = models.DateField(default=date.today)
+    
+    def __str__(self):
+        return self.name
+    
+
--- expenses/urls.py
+++ expenses/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('home/', views.home, name="home"),
+    path('', views.home, name="home"),
     path('expenses/', views.exp_page, name="expenses"),
     path('add/', views.add, name="add"),
     path('update/', views.update, name="update"),
