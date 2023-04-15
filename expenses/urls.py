05df987a2678ec1905aaf06d92cb4382a7aa8a97
Thomas-Longworth <thomaslongworth555@gmail.com>
2023-04-15 17:52:18 +0000

    add css to main body

--- expenses/urls.py
+++ expenses/urls.py
@@ -1,11 +1,13 @@
 from django.urls import path
+from django.contrib.auth.decorators import login_required
 from . import views
 
 urlpatterns = [
     path('', views.home, name="home"),
     path('expenses/', views.exp_page, name="exp_page"),
-    path('add/', views.add, name="add"),
-    path('budget/', views.add_budget, name="budget"),
+    
+    path('add/', login_required(views.add), name="add"),
+    path('budget/', login_required(views.add_budget), name="budget"),
     path('update/<expense_id>', views.update, name="update"),
     path('delete/<expense_id>', views.delete, name="delete"),
 
