df04daee82e0f6af5701cf4b9efd9d912b937bf3
Thomas-Longworth <thomaslongworth555@gmail.com>
2023-04-18 16:35:19 +0000

    add style to main buttons

--- expenses/urls.py
+++ expenses/urls.py
@@ -5,7 +5,6 @@ from . import views
 urlpatterns = [
     path('', views.home, name="home"),
     path('expenses/', views.exp_page, name="exp_page"),
-    
     path('add/', login_required(views.add), name="add"),
     path('budget/', login_required(views.add_budget), name="budget"),
     path('update/<expense_id>', login_required(views.update), name="update"),
